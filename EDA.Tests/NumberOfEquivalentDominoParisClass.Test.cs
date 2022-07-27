a458c372684122ffd337b8cf84a7aadcea1023b9
Carlos Ramos <crperezz92@gmail.com>
2022-07-27 17:58:02 -0500

    Number OfEquivalent Domino Paris

---
+++ EDA.Tests/NumberOfEquivalentDominoParisClass.Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDA.Tests
+{
+    public class NumberOfEquivalentDominoParisClass
+    {
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void NumEquivDominoPairs(int[][] dominoes, int expected)
+        {
+            var result = EDALibrary.NumberOfEquivalentDominoParisClass.NumEquivDominoPairs(dominoes);
+            Assert.Equal(expected, result);
+        }
+
+        public static IEnumerable<object[]> Data =>
+            new List<object[]>
+            {
+                new object[] { new int[][] { new int[] { 1, 2 }, new int[] { 2, 1 }, new int[] { 3, 4 }, new int[] { 5, 6 } }, 1 },
+                new object[] { new int[][] { new int[] { 1, 2 }, new int[] { 1, 2 }, new int[] { 1, 1 }, new int[] { 1, 2 }, new int[] { 2, 2 } }, 3 }
+            };
+    }
+}
---
+++ EDALibrary/NumberOfEquivalentDominoParis.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDALibrary
+{
+    public class NumberOfEquivalentDominoParisClass
+    {
+
+        public static int NumEquivDominoPairs(int[][] dominoes)
+        {
+            var dict = new Dictionary<string, int>();
+            int result = 0;
+
+            for (int i = 0; i < dominoes.Length; i++)
+            {
+                int a = dominoes[i][0];
+                int b = dominoes[i][1];
+                string hash = string.Empty;
+
+                if (a <= b)
+                    hash = $"{a}{b}";
+                else
+                    hash = $"{b}{a}";
+
+                if (dict.ContainsKey(hash))
+                {
+                    dict[hash] ++;
+                }
+                else
+                {
+                    dict.Add(hash, 0);
+                }
+                result += dict[hash];
+            }
+            return result;
+        }
+    }
+}
