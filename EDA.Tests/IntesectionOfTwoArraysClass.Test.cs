b2d1b75e7b5c40897fc3a5f4140440606397d642
Carlos Ramos <crperezz92@gmail.com>
2022-07-27 14:54:13 -0500

    Intesection Of Two Arrays

---
+++ EDA.Tests/IntesectionOfTwoArraysClass.Test.cs
@@ -0,0 +1,14 @@
+﻿namespace EDA.Tests
+{
+    public class IntesectionOfTwoArraysClass
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 2, 1 }, new int[] { 2, 2 }, new int[] {2, 2})]
+        [InlineData(new int[] { 4, 9, 5 }, new int[] { 9, 4, 9, 8, 4 }, new int[] { 9, 4 })]
+        public void Intersect(int[] nums1, int[] nums2, int[] expected)
+        {
+            var result = EDALibrary.IntesectionOfTwoArraysClass.Intersect(nums1, nums2);
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ EDALibrary/IntesectionOfTwoArraysClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDALibrary
+{
+    public class IntesectionOfTwoArraysClass
+    {
+        public static int[] Intersect(int[] nums1, int[] nums2)
+        {
+            var dictionary = new Dictionary<int, int>();
+
+            var result = new LinkedList<int>();
+
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                if(dictionary.ContainsKey(nums1[i]))
+                    dictionary[nums1[i]]++;
+                else
+                    dictionary.Add(nums1[i], 1);
+            }
+
+            for (int i = 0; i < nums2.Length; i++)
+            {                
+
+                if (dictionary.ContainsKey(nums2[i]) && dictionary[nums2[i]] > 0)
+                {
+                    result.AddLast(nums2[i]);
+                    dictionary[nums2[i]]--;
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
