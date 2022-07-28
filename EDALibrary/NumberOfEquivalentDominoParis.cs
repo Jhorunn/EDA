3707aeb2618424845c3654144aff5498e8ce45c0
Carlos Ramos <crperezz92@gmail.com>
2022-07-27 22:21:13 -0500

    Second version of Number Of Equivalent Domino Paris

--- EDALibrary/NumberOfEquivalentDominoParis.cs
+++ EDALibrary/NumberOfEquivalentDominoParis.cs
@@ -13,26 +13,21 @@ namespace EDALibrary
         {
             var dict = new Dictionary<string, int>();
             int result = 0;
+            int a, b;
+            string hash;
 
             for (int i = 0; i < dominoes.Length; i++)
             {
-                int a = dominoes[i][0];
-                int b = dominoes[i][1];
-                string hash = string.Empty;
+                a = dominoes[i][0];
+                b = dominoes[i][1];
 
-                if (a <= b)
-                    hash = $"{a}{b}";
-                else
-                    hash = $"{b}{a}";
+                hash = a <= b ? $"{a}{b}" : $"{b}{a}";
 
-                if (dict.ContainsKey(hash))
-                {
-                    dict[hash] ++;
-                }
-                else
-                {
+                if (dict.ContainsKey(hash))                
+                    dict[hash] ++;                
+                else                
                     dict.Add(hash, 0);
-                }
+                
                 result += dict[hash];
             }
             return result;
